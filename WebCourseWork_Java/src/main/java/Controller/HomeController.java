f9850360f4e60a80bd0c337524b3d64c743ea806
Tenzingo7 <121055545+Tenzingo7@users.noreply.github.com>
2024-04-26 20:30:09 +0545

    Update HomeController.java

--- WebCourseWork_Java/src/main/java/Controller/HomeController.java
+++ WebCourseWork_Java/src/main/java/Controller/HomeController.java
@@ -38,7 +38,25 @@ public class HomeController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/View/home.jsp").forward(request, response);
+		//request.getRequestDispatcher("/WEB-INF/view/home.jsp").forward(request, response);
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3325/mobile_phone" , "root", "root");
+				PhoneDetail phones = new PhoneDetail();
+				List<Product> list = phones.getAllProduct();	
+				request.setAttribute("productObject", list);
+				RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/view/home.jsp");
+				rd.forward(request, response);
+				
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block do post ma haina do get bruh
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 	}
 
 	/**
@@ -47,22 +65,7 @@ public class HomeController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3325/mobile_phone" , "root", "root");
-				PhoneDetail phones = new PhoneDetail();
-				List<Product> list = phones.getAllProduct();	
-				request.setAttribute("productObject", list);
-				RequestDispatcher rd=request.getRequestDispatcher("view/home.jsp");
-				rd.forward(request, response);
-				
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	
 	}
-	}
+}
 
