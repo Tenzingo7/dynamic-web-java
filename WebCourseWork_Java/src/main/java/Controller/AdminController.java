8ad78d09a09e078f1100e9efd4ff4f803338d771
Tenzingo7 <121055545+Tenzingo7@users.noreply.github.com>
2024-04-30 16:57:15 +0545

    Update AdminController.java

--- WebCourseWork_Java/src/main/java/Controller/AdminController.java
+++ WebCourseWork_Java/src/main/java/Controller/AdminController.java
@@ -1,18 +1,23 @@
 package Controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Entity.ProductEntity;
+import Model.ProductModel;
+
 /**
  * Servlet implementation class AdminController
  */
-@WebServlet("/admin")
+@WebServlet(asyncSupported = true, urlPatterns = { "/admin" })
 public class AdminController extends HttpServlet {
-	private static final long serialVersionUID = 1L; 
+	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -27,7 +32,16 @@ public class AdminController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/view/admin.jsp").forward(request, response);
+		ProductModel phones = new ProductModel();
+		List<ProductEntity> list = phones.getAllProduct();
+		//fetch the record form database
+
+		// finally set the list as request attribute
+		request.setAttribute("productObject",list);
+		request.getRequestDispatcher("/WEB-INF/view/Admin.jsp").forward(request, response);
+		System.out.print("heellooooo");
+		System.out.println(list);
+		
 	}
 
 	/**
